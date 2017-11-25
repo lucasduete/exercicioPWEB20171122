e6a4a2bda45c754e5ee4aec760197014b4220951
Lucas Duete <lm.duete@gmail.com>
2017-11-25 20:06:23 -0300

    Finalizando Implementação de ClienteDaoPostgres.

--- src/main/java/com/github/execicio/dao/ClienteDaoPostgres.java
+++ src/main/java/com/github/execicio/dao/ClienteDaoPostgres.java
@@ -1,5 +1,6 @@
 package com.github.execicio.dao;
 
+import com.github.execicio.Enum.EnumAtivo;
 import com.github.execicio.factory.ClienteDaoInterface;
 import com.github.execicio.factory.Conexao;
 import com.github.execicio.model.Cliente;
@@ -10,7 +11,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ClienteDaoPostgres implements ClienteDaoInterface {
 
@@ -21,6 +21,11 @@ public class ClienteDaoPostgres implements ClienteDaoInterface {
 
             String sql = "INSERT INTO Cliente (Nome, Documento, Saldo, Ativo) VALUES (?,?,?,?)";
             PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getDocumento());
+            stmt.setDouble(3, cliente.getSaldo());
+            stmt.setString(4, cliente.getAtivo().toString());
 
             stmt.executeUpdate();
 
@@ -96,16 +101,16 @@ public class ClienteDaoPostgres implements ClienteDaoInterface {
 
             ResultSet rs = stmt.executeQuery();
 
-//            while (rs.next())
-//                (clientes.add(
-//                        new Cliente(
-//                                rs.getInt("id"),
-//                                rs.getString("Nome"),
-//                                rs.getString("Documento"),
-//                                rs.getDouble("Saldo"),
-//                                Enum.valueOf(rs.getString("Ativo"))
-//                        )
-//                )
+            while (rs.next())
+                clientes.add(
+                        new Cliente(
+                                rs.getInt("id"),
+                                rs.getString("Nome"),
+                                rs.getString("Documento"),
+                                rs.getDouble("Saldo"),
+                                EnumAtivo.valueOf(rs.getString("Ativo"))
+                        )
+                );
 
             rs.close();
             stmt.close();
