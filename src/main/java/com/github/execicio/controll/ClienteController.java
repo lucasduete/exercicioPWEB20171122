ea7584bdf3b8f2f221cd1d242b5338004463be1f
Lucas Duete <lm.duete@gmail.com>
2017-11-27 12:05:13 -0300

    Implementando ClienteController.

---
+++ src/main/java/com/github/execicio/controll/ClienteController.java
@@ -0,0 +1,40 @@
+package com.github.execicio.controll;
+
+import com.github.execicio.factory.Fabrica;
+import com.github.execicio.interfaces.ClienteDaoInterface;
+import com.github.execicio.model.Cliente;
+
+import java.util.ArrayList;
+
+public class ClienteController implements ClienteDaoInterface {
+
+    ClienteDaoInterface clienteDao;
+
+    public ClienteController() {
+        clienteDao = new Fabrica().criarDaoPostgres().criarClienteDao();
+    }
+
+    @Override
+    public boolean incluir(Cliente cliente) {
+
+        return clienteDao.incluir(cliente);
+    }
+
+    @Override
+    public boolean alterar(Cliente cliente) {
+        
+        return clienteDao.alterar(cliente);
+    }
+
+    @Override
+    public boolean excluir(Cliente cliente) {
+
+        return clienteDao.excluir(cliente);
+    }
+
+    @Override
+    public ArrayList<Cliente> listar() {
+
+        return clienteDao.listar();
+    }
+}
